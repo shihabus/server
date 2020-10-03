52436524bccde0975396e02d127672263429bfac
Shihabudheen US <shihabudheen@dunzo.in>
2020-10-03 18:14:23 +0530

    custom status

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Work in progress! Hola from VS')
-})
+app.get("/", (req, res) => {
+  res.send("Work in progress! Hola from VS");
+});
+
+app.get("/demo", (req, res) => {
+  res.set("X-full-stack", "4life");
+  res.status(418);
+  res.send("Wakkanda forever");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
